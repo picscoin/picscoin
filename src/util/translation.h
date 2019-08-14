001723dffcc6ff5b2a547994e95ad784a75cfc0a
picscoin <50277586+picscoin@users.noreply.github.com>
2019-08-14 14:16:12 -0400

    Copyrights

--- src/util/translation.h
+++ src/util/translation.h
@@ -1,3 +1,4 @@
+// Copyright (c) 2019 The Bitcoin Core developers
 // Copyright (c) 2019 The Picscoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
